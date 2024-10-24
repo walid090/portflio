6ee7f516c7277f50bcd559befa88400a9a00389c
walid tlili <walid.tlili@sesame.com.tn>
2024-10-24 14:03:13 +0100

    version register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PortflioComponent } from './portflio/portflio.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   { path: '', component: PortflioComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
